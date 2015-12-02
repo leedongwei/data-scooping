4f5f8660c26417c20a1a120fb6cb91bd11a2ed71
DongWei <leedongwei@gmail.com>
2015-12-02 18:56:52 +0800

    Building representative prototypes

--- SBAPUL/bcw.R
+++ SBAPUL/bcw.R
@@ -1,4 +1,4 @@
-libs <- c("e1071", "caret")
+libs <- c("e1071", "caret", "plyr")
 install.packages(libs, dependencies=TRUE)
 lapply(libs, require, character.only=TRUE)
 
@@ -11,27 +11,12 @@ setwd("C:/Users/DongWei/Documents/Projects/data-scooping/data/breast-cancer-wisc
 
 
 
-
-
-################################################
-#### Create Utility functions
-################################################
-
-
-
-
 ################################################
 ####    Loading the data
 ################################################
 bcw <- read.table("breast-cancer-wisconsin.data", sep=",")
-bcw.headers <- c(
-  "id", "V1", 
-  "V2", "V3", 
-  "V4", "V5",
-  "V6", "V7",
-  "V8", "V9",
-  "class"
-)
+bcw.headers <- c( "id", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "class")
+bcw.featureHeaders <- c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9")
 names(bcw) <- bcw.headers
 
 
@@ -59,18 +44,20 @@ V6.mean <- temp / length(bcw$V6)
 ## Replace missing V6 values with mean
 bcw$V6[index] <- V6.mean
 
+
+
 ################################################
 ####    Splitting the data
 ################################################
-partitionIndex <- createDataPartition(
+temp <- createDataPartition(
                     bcw$class, 
                     times = 1,
                     p = 0.6,
                     list = FALSE)
 
 ## 60% is set as training set, 40% for test set
-bcw.trn <- bcw[partitionIndex, ]
-bcw.tst <- bcw[-partitionIndex, ]
+bcw.trn <- bcw[temp, ]
+bcw.tst <- bcw[-temp, ]
 
 
 ## Class "4" (malignant) is the positive set
@@ -79,16 +66,20 @@ bcw.trn.negative <- subset(bcw.trn, class=="2")
 
 ## Value of g is taken from page 1581 of this paper
 g = 0.15
-partitionIndex <- createDataPartition(
+temp <- createDataPartition(
                     bcw.trn.positive$class, 
                     times = 1,
                     p = g,
                     list = FALSE)
 
 ## Set up PS and US
-bcw.trn.PS <- bcw.trn.positive[partitionIndex, ]
-bcw.trn.US <- bcw.trn.positive[-partitionIndex, ]
+bcw.trn.PS <- bcw.trn.positive[temp, ]
+bcw.trn.US <- bcw.trn.positive[-temp, ]
 bcw.trn.US <- rbind(bcw.trn.US, bcw.trn.negative)
+
+## Delete variables that are never again used
+## Prevents confusion in Global Env
+rm(bcw.trn.positive, bcw.trn.negative)
 
 
 
@@ -104,17 +95,17 @@ bcw.trn.spy.US$isSpy <- FALSE
 
 ## Value of s is taken from page 3 of Liu, Dai, Li, Lee, Yu (2003)
 s = 0.15
-partitionIndex <- createDataPartition(
+temp <- createDataPartition(
                     bcw.trn.spy.PS$class, 
                     times = 1,
                     p = s,
                     list = FALSE)
 
 ## Put spies into US
-temp <- bcw.trn.spy.PS[partitionIndex, ]
+temp <- bcw.trn.spy.PS[temp, ]
 temp$isSpy <- TRUE
 bcw.trn.spy.US <- rbind(temp,bcw.trn.spy.US)
-bcw.trn.spy.PS <- bcw.trn.spy.PS[-partitionIndex, ]
+bcw.trn.spy.PS <- bcw.trn.spy.PS[-temp, ]
 
 bcw.trn.spy.PS$spyLabel <- 4
 bcw.trn.spy.US$spyLabel <- 2
@@ -157,6 +148,8 @@ bcw.trn.spy.POSITIVE <- bcw.trn.spy.US[bcw.trn.spy.US$Pr >= th, ]
 bcw.trn.spy.NEGATIVE$spyLabel <- as.numeric(levels(bcw.trn.spy.NEGATIVE$spyLabel))[bcw.trn.spy.NEGATIVE$spyLabel]
 bcw.trn.spy.POSITIVE$spyLabel <- as.numeric(levels(bcw.trn.spy.POSITIVE$spyLabel))[bcw.trn.spy.POSITIVE$spyLabel]
 
+
+
 ################################################
 ####    Rocchio
 ################################################
@@ -164,18 +157,13 @@ bcw.trn.spy.POSITIVE$spyLabel <- as.numeric(levels(bcw.trn.spy.POSITIVE$spyLabel
 bcw.trn.roc.PS <- bcw.trn.PS
 bcw.trn.roc.US <- bcw.trn.US
 
-CVector <- function(DF1, DF2) {
+BcwRocchioVectorBuilder <- function(DF1, DF2) {
   alpha <- 16
   beta <- 4
   
-  ## remove columns that are not significant
-  ## TODO: these columns are specific towards BCW data
-  DF1$id <- NULL
-  DF1$class <- NULL
-  DF1$rocLabel <- NULL
-  DF2$id <- NULL
-  DF2$class <- NULL
-  DF2$rocLabel <- NULL
+  ## Remove non-significant columns (e.g. id, class, rocLabel)
+  DF1 <- DF1[ , bcw.featureHeaders]
+  DF2 <- DF2[ , bcw.featureHeaders]
   
   norma.d.1 <- apply(DF1, 1, function(x){x/sqrt(sum(x^2))})
   DF1.size <- nrow(DF1)
@@ -189,14 +177,15 @@ CVector <- function(DF1, DF2) {
   return(c)
 }
 
-LabelData <- function(DF.row, c.1, c.2) {
-  DF.row$id <- NULL
-  DF.row$class <- NULL
-  DF.row$rocLabel <- NULL
-  r.1 <- sum(c.1 * DF.row)
-  r.2 <- sum(c.2 * DF.row)
+BcwRocchioClassifer <- function(DF.row, vector1, vector2) {
   
-  if (r.1 >= r.2) {
+  ## Remove non-significant columns
+  DF.row <- DF.row[ , bcw.featureHeaders]
+  
+  r1 <- sum(vector1 * DF.row)
+  r2 <- sum(vector2 * DF.row)
+  
+  if (r1 >= r2) {
     rocLabel <- 4
   } else {
     rocLabel <- 2
@@ -205,12 +194,12 @@ LabelData <- function(DF.row, c.1, c.2) {
   return(rocLabel)
 }
 
-rocchio.positiveVector <- CVector(bcw.trn.roc.PS, bcw.trn.roc.US)
-rocchio.negativeVector <- CVector(bcw.trn.roc.US, bcw.trn.roc.PS)
+rocchio.posVector <- BcwRocchioVectorBuilder(bcw.trn.roc.PS, bcw.trn.roc.US)
+rocchio.negVector <- BcwRocchioVectorBuilder(bcw.trn.roc.US, bcw.trn.roc.PS)
 
 bcw.trn.roc.US$rocLabel <- 0
 for (i in 1:nrow(bcw.trn.roc.US)) {
-  bcw.trn.roc.US[i,]$rocLabel <- LabelData(bcw.trn.roc.US[i,], rocchio.positiveVector, rocchio.negativeVector)
+  bcw.trn.roc.US[i,]$rocLabel <- BcwRocchioClassifer(bcw.trn.roc.US[i,], rocchio.posVector, rocchio.negVector)
 }
 
 ## Extract data which is marked negative
@@ -228,24 +217,60 @@ bcw.trn.roc.POSITIVE <- subset(bcw.trn.roc.US, rocLabel=="4")
 
 temp <- merge(bcw.trn.spy.NEGATIVE[ ,c(1:11, 13)], bcw.trn.roc.NEGATIVE[ ,c(1, 12)], by="id")
 bcw.trn.step1.NS <- c()
-bcw.trn.step1.US <- c()
 for (i in 1:nrow(temp)) {
   if ((temp[i,]$spyLabel == 2) && (temp[i,]$rocLabel == 2)) {
     bcw.trn.step1.NS <- rbind(bcw.trn.step1.NS, temp[i, ])
-  } else {
-    bcw.trn.step1.US <- rbind(bcw.trn.step1.US, temp[i, ])
   }
 }
 
-## Clean up Global Env
+bcw.trn.step1.PS <- bcw.trn.PS
+bcw.trn.step1.US <- subset(bcw.trn.US, !(bcw.trn.US$id %in% bcw.trn.step1.NS$id))
+
+
+
+################################################
+####    Clean up Global Env
+################################################
 rm(bcw.trn.roc.POSITIVE, bcw.trn.roc.NEGATIVE, bcw.trn.roc.PS, bcw.trn.roc.US)
 rm(bcw.trn.spy.POSITIVE, bcw.trn.spy.NEGATIVE, bcw.trn.spy.PS, bcw.trn.spy.US)
+rm(index, libs, V6.mean, nbc, rocchio.posVector, rocchio.negVector)
+
 
 
 ################################################
 ####    Clustering
 ################################################
-# t = 30
-# m = t * nrow(bcw.trn.NS) / (nrow(bcw.trn.US) + nrow(bcw.trn.NS))
+t = 30
+m = floor(t * nrow(bcw.trn.step1.NS) / (nrow(bcw.trn.step1.US) + nrow(bcw.trn.step1.NS)))
 
-# bcw.cluster <- kmeans(bcw.trn.NS, m)
+bcw.trn.step1.NS.fit <- kmeans(bcw.trn.step1.NS[, bcw.featureHeaders], m)
+
+## Label data with cluster number
+bcw.trn.step1.NS <- data.frame(bcw.trn.step1.NS, bcw.trn.step1.NS.fit$cluster)
+bcw.trn.step1.NS <- rename(bcw.trn.step1.NS, c("bcw.trn.step1.NS.fit.cluster" = "cluster"))
+
+
+
+################################################
+####    Build Representative Prototypes
+################################################
+temp <- matrix(0, ncol = 10, nrow = 0)
+bcw.trn.step1.p.k <- data.frame(temp)
+bcw.trn.step1.n.k <- data.frame(temp)
+
+for (k in 1:m) {
+  cluster.k <- subset(bcw.trn.step1.NS, cluster==k)
+  
+  p.k <- BcwRocchioVectorBuilder(bcw.trn.step1.PS, cluster.k)
+  p.k <- c(k, p.k)
+  bcw.trn.step1.p.k <- rbind(bcw.trn.step1.p.k, p.k)
+  
+  n.k <- BcwRocchioVectorBuilder(cluster.k, bcw.trn.step1.PS)
+  n.k <- c(k, n.k)
+  bcw.trn.step1.n.k <- rbind(bcw.trn.step1.n.k, n.k)
+}
+rm(cluster.k, p.k, n.k)
+
+names(bcw.trn.step1.p.k) <- c("k", bcw.featureHeaders)
+names(bcw.trn.step1.n.k) <- c("k", bcw.featureHeaders)
+
