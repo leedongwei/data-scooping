9dc9a2833ecf4a56f74e07aa473443dc331adef8
DongWei <leedongwei@gmail.com>
2016-03-28 16:41:26 +0800

    Bug fix to Spy-EM

--- newsgroup-SpyEm.R
+++ newsgroup-SpyEm.R
@@ -1,7 +1,7 @@
 source("newsgroup-utils-functions.R")
 
 
-ngp.model.Spy_EM <- function (ngp.trnMatrix, ngp.class) {
+ngp.model.Spy_EM <- function (ngp.dtm, ngp.class) {
 
   PS <- rownames(ngp.class[ngp.class$label == 1, ])
   US <- rownames(ngp.class[ngp.class$label == -1, ])
@@ -20,24 +20,24 @@ ngp.model.Spy_EM <- function (ngp.trnMatrix, ngp.class) {
 
 
 
-  RN <- ngp.model.Spy_EM.FindRN(ngp.trnMatrix, ngp.class)
+  RN <- ngp.model.Spy_EM.FindRN(ngp.dtm, ngp.class)
   ngp.class$RN <- 0
   ngp.class[RN, ]$RN <- -1
 
-  model.Spy_EM <- ngp.model.Spy_EM.BuildModel(ngp.trnMatrix, ngp.class)
+  model.Spy_EM <- ngp.model.Spy_EM.BuildModel(ngp.dtm, ngp.class)
 
   return(model.Spy_EM)
 }
 
-ngp.model.Spy_EM.FindRN <- function (ngp.trnMatrix, ngp.class) {
+ngp.model.Spy_EM.FindRN <- function (ngp.dtm, ngp.class) {
 
   PS <- rownames(ngp.class[ngp.class$label == 1, ])
   US <- rownames(ngp.class[ngp.class$label == -1, ])
   US.spies <- rownames(ngp.class[ngp.class$isSpy == TRUE, ])
 
   ## Check that matrix and class are sorted
-  if (all(rownames(ngp.trnMatrix) != rownames(ngp.class))) {
-    ngp.trnMatrix <- ngp.trnMatrix[order(rownames(ngp.trnMatrix)), ]
+  if (all(rownames(ngp.dtm) != rownames(ngp.class))) {
+    ngp.dtm <- ngp.dtm[order(rownames(ngp.dtm)), ]
     ngp.class     <- ngp.class[order(rownames(ngp.class)), ]
   }
   PS <- PS[order(PS)]
@@ -51,18 +51,18 @@ ngp.model.Spy_EM.FindRN <- function (ngp.trnMatrix, ngp.class) {
 
   ##############################################
   ####    Naive-Bayes iterations
-  nBayes.model <- naiveBayes(ngp.trnMatrix, ngp.class$predict, laplace=0.15)
-  US.predict <- predict(nBayes.model, ngp.trnMatrix[US, ])
+  nBayes.model <- naiveBayes(ngp.dtm, ngp.class$predict, laplace=0.15)
+  US.predict <- predict(nBayes.model, ngp.dtm[US, ])
   var.iter <- 1
   var.iter.values <- c(0, 0, 0)
 
   while (var.iter < 25) {
-    utils.cat(paste("        Spy_EM.FindRN, iteration #", var.iter, "\n", sep=""))
+    utils.cat(paste("        Spy_EM.FindRN thread #", var.j, ", iteration #", var.iter, "\n", sep=""))
 
     ngp.class[US, ]$predict <- US.predict
 
-    nBayes.model <- naiveBayes(ngp.trnMatrix, ngp.class$predict, laplace=0.1)
-    US.predict <- predict(nBayes.model, ngp.trnMatrix[US, ])
+    nBayes.model <- naiveBayes(ngp.dtm, ngp.class$predict, laplace=0.1)
+    US.predict <- predict(nBayes.model, ngp.dtm[US, ])
     var.iter <- var.iter + 1
 
     ## Stopping condition
@@ -84,11 +84,11 @@ ngp.model.Spy_EM.FindRN <- function (ngp.trnMatrix, ngp.class) {
       }
     }
   }
-  utils.cat(paste("        Spy_EM.FindRN, break at #", var.iter, "\n", sep=""))
+  utils.cat(paste("        Spy_EM.FindRN:  thread #", var.j, ", break at #", var.iter, "\n", sep=""))
 
   ## Run final mode on PS and US to get Probability
-  spies.predict <- predict(nBayes.model, ngp.trnMatrix[US.spies, ], type='raw')
-  US.predict <- predict(nBayes.model, ngp.trnMatrix[US, ], type='raw')
+  spies.predict <- predict(nBayes.model, ngp.dtm[US.spies, ], type='raw')
+  US.predict <- predict(nBayes.model, ngp.dtm[US, ], type='raw')
 
   ## Extract RN and mark it in ngp.class
   ## TODO: Find out why threshold is zero
@@ -102,7 +102,7 @@ ngp.model.Spy_EM.FindRN <- function (ngp.trnMatrix, ngp.class) {
 }
 
 
-ngp.model.Spy_EM.BuildModel <- function (ngp.trnMatrix, ngp.class) {
+ngp.model.Spy_EM.BuildModel <- function (ngp.dtm, ngp.class) {
 
   ## Move spies back into PS
   US.spies <- rownames(ngp.class[ngp.class$isSpy == TRUE, ])
@@ -115,40 +115,39 @@ ngp.model.Spy_EM.BuildModel <- function (ngp.trnMatrix, ngp.class) {
   QS <- US [! US %in% RN]
 
   ## Set for iterations
-  ngp.class$predict <- 0
+  ngp.class$predict <- -1
   ngp.class[PS, ]$predict <- 1
-  ngp.class[RN, ]$predict <- -1
   ngp.class$predict <- as.factor(ngp.class$predict)
 
   ## First run of Naive Bayes
   nBayes.model <- naiveBayes(
-    ngp.trnMatrix[c(PS, RN), ],
+    ngp.dtm[c(PS, RN), ],
     ngp.class[c(PS, RN), ]$predict,
     laplace=0.1)
-  QS.predict <- predict(nBayes.model, ngp.trnMatrix[QS, ])
+  QS.predict <- predict(nBayes.model, ngp.dtm[QS, ])
   var.iter <- 1
   var.iter.values <- c(0, 0, 0)
 
   while(var.iter < 25) {
-    utils.cat(paste("        Spy_EM.BuildModel, iteration #", var.iter, "n", sep=""))
+    utils.cat(paste("        Spy_EM.BuildModel: thread #", var.j, ", iteration #", var.iter, "\n", sep=""))
 
     ngp.class[QS, ]$predict <- QS.predict
 
-    nBayes.model <- naiveBayes(ngp.trnMatrix, ngp.class$predict, laplace=0.1)
-    QS.predict <- predict(nBayes.model, ngp.trnMatrix[QS, ])
+    nBayes.model <- naiveBayes(ngp.dtm, ngp.class$predict, laplace=0.1)
+    QS.predict <- predict(nBayes.model, ngp.dtm[QS, ])
+    var.iter <- var.iter + 1
 
     ## Stopping condition
-    if (identical(ngp.class[US, ]$predict, US.predict)) {
+    if (identical(ngp.class[QS, ]$predict, QS.predict)) {
       break
     } else {
-      # print(which(ngp.class[US, ]$predict != US.predict))
       var.iter.values[3] <- var.iter.values[2]
       var.iter.values[2] <- var.iter.values[1]
-      var.iter.values[1] <- length(which(ngp.class[US, ]$predict != US.predict))
+      var.iter.values[1] <- length(which(ngp.class[QS, ]$predict != QS.predict))
 
       ## If the number of different predictions between last 3 iterations is less than 10%, we can assume that it has converged
       # print(var.iter.values)
-      var.maxDiff <- ceiling(max(var.iter.values) / 10)
+      var.maxDiff <- ceiling(min(var.iter.values) / 10)
       if (!(0 %in% var.iter.values)
           & (var.iter.values[3] - var.iter.values[2] < var.maxDiff)
           & (var.iter.values[2] - var.iter.values[1] < var.maxDiff)) {
@@ -156,6 +155,6 @@ ngp.model.Spy_EM.BuildModel <- function (ngp.trnMatrix, ngp.class) {
       }
     }
   }
-  utils.cat(paste("        Spy_EM.BuildModel, break at #", var.iter, "n", sep=""))
+  utils.cat(paste("        Spy_EM.BuildModel:  thread #", var.j, ", break at #", var.iter, "\n", sep=""))
   return(nBayes.model)
 }
